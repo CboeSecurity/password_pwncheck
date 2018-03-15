ff449cc1f4176e5c6032e69d48b350410c3fccce
root <root@lx-node9.bats.com>
2018-03-15 14:42:19 -0500

    Updated for support for remote GUI and regex blacklist

--- pwned_password_server/pwned-password-server.py
+++ pwned_password_server/pwned-password-server.py
@@ -8,6 +8,9 @@
 # then in your browser, visit:
 #    https://localhost:4443
 
+from platform import system
+import argparse
+
 from math import ceil,floor
 from os import listdir
 from os.path import isfile, join, getsize
@@ -28,17 +31,7 @@ import ssl
 from urlparse import urlparse, parse_qs
 from json import dumps
 
-SSLCertFile='./localhost.cer'
-SSLKeyFile='./localhost.key'
-SSLKeyFilePassword='<password>'
-
-ListenPort = 443
-ListenInterface = '0.0.0.0'
-BreachedPasswordPath = './db'
-TokenDatabasePath = 'tokens.sqlite'
-debug = False
-
-fp = open(u'/var/log/pwnedpass-access.log','a')  
+#fp = open(u'/var/log/pwnedpass-access.log','a')  
 HashAlgorithmStrings = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
 
 ## SQLITE Schema
@@ -48,7 +41,7 @@ HashAlgorithmStrings = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
 ##  searchfile => binary search against an ordered file  ##
 ###########################################################
 class SearchFile:
-    def __init__(self,filepath):
+    def __init__(self,filepath,debug=False):
         self.filepath = filepath
         self.fp = open(self.filepath,'r')
 
@@ -57,10 +50,11 @@ class SearchFile:
         self.nows_linesize = len(first.strip())
         self.filesize = getsize(self.filepath)
         self.filerows = self.filesize/self.linesize
+        self.debug = debug
 
 
     def find(self,string):
-        if debug:
+        if self.debug:
             print "   * searchfile: find: filepath=%s"%(self.filepath)
         string = string.strip()
         if len(string) != self.nows_linesize:
@@ -75,7 +69,7 @@ class SearchFile:
     def _find(self,needle,currow,jumpsize,prevjumpsize=0):
         newjumpsize = ceil(float(jumpsize)/2)
         if jumpsize == 0 or newjumpsize == prevjumpsize:
-            if debug:
+            if self.debug:
                 print "     + %d: No Match"%(currow)
             return None
         self.fp.seek(currow*self.linesize)
@@ -85,7 +79,7 @@ class SearchFile:
         elif curstring < needle:
             return self._find(needle,min(self.filerows-1,currow+jumpsize),newjumpsize,jumpsize)
         else:
-            if debug:
+            if self.debug:
                 print "    \- - %d: %s == %s (%d)"%(currow,curstring,needle,jumpsize)
             return currow
 
@@ -113,12 +107,13 @@ class SearchFiles:
 
 
 class TokenCheck:
-    def __init__(self,dbpath):
+    def __init__(self,dbpath,debug=False):
         self.conn = sqlite3.connect(dbpath)
         self.checkstr = 'SELECT count(*) FROM tokens WHERE token=?'
         self.insertstr = "INSERT INTO tokens (user,token,ip) VALUES ( ?, ?, ? )"
         self.createstr = 'CREATE TABLE tokens ( user TEXT, token TEXT, ip TEXT, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL);'
         self.indexstr = 'CREATE INDEX idx ON tokens(token);'
+        self.debug = debug
 
     def addToken(self,user,token,ip=None):
         c = self.conn.cursor()
@@ -134,7 +129,7 @@ class TokenCheck:
                 c.execute(self.createstr)
                 c.execute(self.indexstr)
                 self.conn.commit()
-                if debug == True:
+                if self.debug == True:
                     print "Committed new table into database"
                 return self.checkToken(user,token)
         res =  c.fetchone()[0]
@@ -165,47 +160,45 @@ class PasswordRequestHandler(SimpleHTTPRequestHandler):
 
     pwned = None
     tokendb = None
+    loghandle = None
+    debug = False
+    regexs = []
 
     def do_GET(self):
-        if debug:
+        if self.debug:
             print "Received Request"
         
-        #fp.write((u"HEXREQUEST:%s:HEXREQUEST\n"%self.path.encode('hex')))   
-        #fp.flush()
-        #fp.write((u"REQUEST:%s:REQUEST\n"%self.path.encode('utf8')))
         parsed_path = urlparse(unicode(self.path))
         args = parse_qs(parsed_path.query)
-        #if 'debug' in args:
-        #    debug = True
         self.user = "-"
         self.retval = "-"
         self.code = -1
         
         if parsed_path.path == "/checkpwd":
             message = ''
-            if u'debug' in args:
-                message_parts = [
-                    u'CLIENT VALUES:',
-                    u'client_address=%s (%s)' % (self.client_address,
-                                                self.address_string()),
-                    u'command=%s' % self.command,
-                    u'path=%s' % self.path,
-                    u'real path=%s' % parsed_path.path,
-                    u'query=%s' % parsed_path.query,
-                    u'arguments (json)=%s' % dumps(args),
-                    u'request_version=%s' % self.request_version,
-                    u'',
-                    u'SERVER VALUES:',
-                    u'server_version=%s' % self.server_version,
-                    u'sys_version=%s' % self.sys_version,
-                    u'protocol_version=%s' % self.protocol_version,
-                    u'',
-                    u'HEADERS RECEIVED:',
-                    ]
-                for name, value in sorted(self.headers.items()):
-                    message_parts.append('%s=%s' % (name, value.rstrip()))
-                message_parts.append('')
-                message += u'\r\n'.join(message_parts)
+#            if u'debug' in args:
+#                message_parts = [
+#                    u'CLIENT VALUES:',
+#                    u'client_address=%s (%s)' % (self.client_address,
+#                                                self.address_string()),
+#                    u'command=%s' % self.command,
+#                    u'path=%s' % self.path,
+#                    u'real path=%s' % parsed_path.path,
+#                    u'query=%s' % parsed_path.query,
+#                    u'arguments (json)=%s' % dumps(args),
+#                    u'request_version=%s' % self.request_version,
+#                    u'',
+#                    u'SERVER VALUES:',
+#                    u'server_version=%s' % self.server_version,
+#                    u'sys_version=%s' % self.sys_version,
+#                    u'protocol_version=%s' % self.protocol_version,
+#                    u'',
+#                    u'HEADERS RECEIVED:',
+#                    ]
+#                for name, value in sorted(self.headers.items()):
+#                    message_parts.append('%s=%s' % (name, value.rstrip()))
+#                message_parts.append('')
+#                message += u'\r\n'.join(message_parts)
            
             if 'u' in args and 'p' in args:
                 user = unquote(args['u'][0]) #.decode('utf8'); 
@@ -216,16 +209,40 @@ class PasswordRequestHandler(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(message)
+        elif parsed_path.path == "/test":
+            self.send_response(200)
+            self.end_headers()
+        elif parsed_path.path == "/":
+            form = open('form.html','r');
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(form.read())
         else:
+            print parsed_path.path
+            return
             self.send_response(301)
             self.send_header('Location', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
             self.end_headers()
         return
 
+    def isRegexBlacklistMatch(self,user,password):
+        for line in self.regexs:
+            (ignore,regex) = line
+            regex = regex.replace('<user>',user)
+            results = None
+            if ignore == True:
+                results = re.search(regex,password,re.IGNORECASE)
+            else:
+                results = re.search(regex,password)
+            if results != None:
+                return True
+        return False
+
     def verifyPasswordGood(self,user,password):
         isPwned = False
         isUsed = False
         tooShort = False
+        isRegex = False
         reason = []
         retval = False
    
@@ -237,69 +254,109 @@ class PasswordRequestHandler(SimpleHTTPRequestHandler):
         
             ret = self.pwned.find(testhash)
             if ret == None:
-                if debug:
+                if self.debug:
                     print " * %s: %s: * No Match"%(algstr, testhash)
             else:
                 isPwned = True
-                if debug:
+                if self.debug:
                     print " *** %s: %s: - Breached! %s ***"%(algstr, testhash,ret)
                 reason.append( "Password can be found in the breached password database" )
-    
+  
+        # test for bad matches against canned regex statements:
+        if self.isRegexBlacklistMatch(user,password):
+            if self.debug:
+                print " \- - Password matches blacklist terms"
+            isRegex = True
+            reason.append( "Password matches blacklist terms" )
+
+
         ### now test for bad token usage
         token = self.tokendb.tokenizePassword(password)
         tokenhash = self.tokendb.hashToken(token) 
-        if debug:
+        if self.debug:
             print " \- * Password: %s"%(password)
             print " \- * Token: %s"%(token)
 
         if len(token) < 15:
-            if debug:
+            if self.debug:
                 print " \- - Password is too short (%d, must be 15 character minimum)"%(len(token))
             tooShort = True
             reason.append( "Password is too short (%d, must be 15 character minimum)"%(len(token)) )
 
         if self.tokendb.checkToken(user,tokenhash):
-            if debug:
+            if self.debug:
                 print " \- - Password is in token library"
             isUsed = True
             reason.append( "Password is too similar to a previously used password" )
         else:
-            if debug:
+            if self.debug:
                 print " \- * Password is not in token library"
     
-        if not isUsed and not isPwned and not tooShort:
-            if debug:
+        if not isUsed and not isPwned and not tooShort and not isRegex:
+            if self.debug:
                 self.tokendb.addToken(user,tokenhash,self.client_address[0])
                 print " \- + Password is a valid entry and is now reserved"
             reason.append( "Password is valid and now reserved" )
             retval = True
         else:
-            if debug:
+            if self.debug:
                 print " \- - Password is invalid and unacceptable"
             retval = False
         
-        self.code = int(isPwned)*100+int(isUsed)*10+int(tooShort)*1
+        self.code = int(isPwned)*100+int(isUsed)*10+int(tooShort)*1+int(isRegex)*20
         self.retval = retval
         return (retval,self.code,'\n'.join(reason))
 
     def log_message(self, format, *args):
-        if not debug:
+        if not self.debug:
             args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1],args[2])
         print "%s\t%s\t%s\t%s\t%d\t%s" % (self.log_date_time_string(), self.client_address[0],self.user,self.retval,self.code,format%args)
-        fp.write("%s,%s,%s,%s,%d,%s\n" % (self.log_date_time_string(), self.client_address[0],self.user,self.retval,self.code,format%args))
-        fp.flush()
+        if self.loghandle:
+            self.loghandle.write("%s,%s,%s,%s,%d,%s\n" % (self.log_date_time_string(), self.client_address[0],self.user,self.retval,self.code,format%args))
+            self.loghandle.flush()
 
 if __name__ == '__main__':
+
+    args = argparse.ArgumentParser()
+    args.add_argument('-c','--sslcert',help='SSL Public Certificate for the HTTPS Web Server')
+    args.add_argument('-k','--sslkey',help='SSL Private Key for the HTTPS Web Server')
+    args.add_argument('--sslkeypass',help='Passphrase to decrypt the SSL Private Key', default=None)
+    args.add_argument('-p','--port',help='TCP Port to for web server to listen on', default=443, type=int)
+    args.add_argument('-i','--interface',help='TCP Bind interface for web server to listen on', default='0.0.0.0')
+    args.add_argument('-b','--breachdir',help='Breached passwords hash directory path',default='./db')
+    args.add_argument('-d','--dbpath',help='Path to the sqlite file store for used passwords',default='tokens.sqlite')
+    args.add_argument('-B','--blacklistpath',help='Path to a blacklist file of bad regex strings in passwords.  Lines starting with "i:" are case insensitively matched.  "<user>" will be replaced with the username',default=None)
+    if system() == 'Windows':
+        args.add_argument('-l','--logpath',help='Path to the logfile',default=None)
+    else:
+        args.add_argument('-l','--logpath',help='Path to the logfile',default='/var/log/pwnedpass-access.log')
+    args.add_argument('-D','--debug',help='Enter Debug mode (***DO NOT USE IN PRODUCTION!***)',action='store_true')
+    cfg = args.parse_args()
+
+    print cfg.sslcert
+
     # build up our SSL wrapper with our keying material
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-    context.load_cert_chain(certfile=SSLCertFile, keyfile=SSLKeyFile, password=SSLKeyFilePassword)
+    context.load_cert_chain(certfile=cfg.sslcert, keyfile=cfg.sslkey, password=cfg.sslkeypass)
     
     # Setup our database accesses
-    PasswordRequestHandler.pwned = SearchFiles(BreachedPasswordPath)
-    PasswordRequestHandler.tokendb = TokenCheck(TokenDatabasePath)
-    
+    PasswordRequestHandler.debug = cfg.debug
+    PasswordRequestHandler.pwned = SearchFiles(cfg.breachdir)
+    PasswordRequestHandler.tokendb = TokenCheck(cfg.dbpath)
+    PasswordRequestHandler.loghandle = open(unicode(cfg.logpath,'utf8'),'a')
+   
+    if cfg.blacklistpath != None:
+        fp = open(cfg.blacklistpath,'r')
+        for line in fp:
+            line = line.strip()
+            insensitive = False
+            if line[0:2] == "i:":
+                insensitive = True
+                line = line[2:]
+            PasswordRequestHandler.regexs.append((insensitive,line))
+   
     # create the web server, glue in SSL
-    httpd = HTTPServer((ListenInterface, ListenPort), PasswordRequestHandler) 
+    httpd = HTTPServer((unicode(cfg.interface), cfg.port), PasswordRequestHandler) 
     httpd.socket = context.wrap_socket (httpd.socket, server_side=True)
     
     # Fire off the web server to start accepting requests
