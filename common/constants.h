7c436215d69643c7b0458bf6fb88a2f6af4c737f
Jan Grzymala-Busse <jbusse@cboe.com>
2019-07-30 11:50:48 -0500

    Added code changes to reuse code

---
+++ common/constants.h
@@ -0,0 +1,12 @@
+/*
+ * Authored by: Jan Grzymala-Busse 
+ * Cboe Security
+ * Provided as is, no warranties, please see included licensing.
+*/
+
+// our code 
+#define PASSWORD_URL_ENV "KERB_PASSWORD_URL"
+#define DEFAULT_PASSWORD_URL "https://localhost/checkpwd?u=%s&p=%s";   
+
+#define CURL_RESPONSE_INIT_SIZE 1024 
+#define CURL_MAX_BUFLEN 2048
