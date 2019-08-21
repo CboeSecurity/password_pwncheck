8f6afa88af3d0019f40494896653276ccb256876
CboeSecurity <jbusse@cboe.com>
2019-08-21 15:19:51 -0500

    removed debugging from release

--- kerberos/build-release.sh
+++ kerberos/build-release.sh
@@ -5,9 +5,9 @@
 #ld -x --shared -o pwncheck.so bin/krb_password_pwncheck.o bin/config.o bin/curl.o -lcurl -lcrypto && mv pwncheck.so /lib/security/
 #cp /lib/security/pwncheck.so /usr/lib/x86_64-linux-gnu/krb5/plugins/pwqual/
 
-gcc -g -shared -fPIC -fno-stack-protector -c src/krb_password_pwncheck.c -lcurl -lcrypto -o bin/krb_password_pwncheck.o  && \
-gcc -g -shared -fPIC -fno-stack-protector -c src/config.c -lcurl -lcrypto -o bin/config.o && \
-gcc -g -shared -fPIC -fno-stack-protector -c ../common/curl.c -lcurl -lcrypto -o bin/curl.o && \
-gcc -g -o bin/harness src/harness.c -ldl
-ld -g -x --shared -o bin/pwncheck.so bin/krb_password_pwncheck.o bin/config.o bin/curl.o -lcurl -lcrypto -lyaml
+gcc -shared -fPIC -fno-stack-protector -c src/krb_password_pwncheck.c -lcurl -lcrypto -o bin/krb_password_pwncheck.o  && \
+gcc -shared -fPIC -fno-stack-protector -c src/config.c -lcurl -lcrypto -o bin/config.o && \
+gcc -shared -fPIC -fno-stack-protector -c ../common/curl.c -lcurl -lcrypto -o bin/curl.o && \
+gcc -o bin/harness src/harness.c -ldl
+ld -x --shared -o bin/pwncheck.so bin/krb_password_pwncheck.o bin/config.o bin/curl.o -lcurl -lcrypto -lyaml
 cp bin/pwncheck.so /usr/lib/x86_64-linux-gnu/krb5/plugins/pwqual/
