d1af874bc0c8091d6063c79c44a7fafcc5f02b4f
Cboe Security <jbusse@cboe.com>
2020-01-10 11:03:35 -0600

    indent fix

--- pwned_password_server/password-pwncheck.py
+++ pwned_password_server/password-pwncheck.py
@@ -39,19 +39,19 @@ def logmsg(request,type,message,args):
 
 def getLogDateTime(args):
     if not cfg.debug:
-         try:
-             if len(args) == 3:
-                 args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1],args[2])
-             elif len(args) == 2:
-                 args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1])
-         except TypeError as e:
-             print("arg0: %s"%type(args[0]))
-             print("arg1: %s"%type(args[1]))
-             if len(args) == 3:
-                 print("arg2: %s"%type(args[2]))
-             print(args)
-             raise e
-#        args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1],args[2])
+        try:
+            if len(args) == 3:
+                args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1],args[2])
+            elif len(args) == 2:
+                args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1])
+        except TypeError as e:
+            print("arg0: %s"%type(args[0]))
+            print("arg1: %s"%type(args[1]))
+            if len(args) == 3:
+                print("arg2: %s"%type(args[2]))
+            print(args)
+            raise e
+#       args = (re.sub(r'p=.* HTTP',r'p=<redacted> HTTP',args[0]),args[1],args[2])
     return args
 
 #"/" "GET" -> form.html
